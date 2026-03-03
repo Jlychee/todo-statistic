2c8b3d2613449a92a5ca61dd496665ba2d30c33a
sofjushka <anonimks13@gmail.com>
2026-03-03 20:06:18 +0500

    done 4

--- index.js
+++ index.js
@@ -19,11 +19,24 @@ function getComments() {
             const match = line.match(/\/\/\s*TODO\s*(.*)/);
             if (match) {
                 const commentText = match[1];
+                const parts = commentText.split(';')
+
                 const isImportant = commentText.includes('!');
-                comments.push({
+                if (parts.length === 3){
+                    comments.push({
+                    text: parts[2],
+                    isImportant : isImportant, 
+                    author: parts[0], 
+                    data : parts[1],
+                })} else {
+                    comments.push({
                     text: commentText,
-                    isImportant : isImportant
+                    isImportant : isImportant, 
+                    author: NaN, 
+                    data : NaN,
                 });
+                }
+                
             }
         }
     }
@@ -31,8 +44,10 @@ function getComments() {
 }
 
 function processCommand(command) {
+    const parts = command.trim().split(/\s+/);
+    const cmd = parts[0].toLowerCase();
     const lines = getComments()
-    switch (command) {
+    switch (cmd) {
         case 'exit':
             process.exit(0);
             break;
@@ -49,6 +64,20 @@ function processCommand(command) {
                 }
             }
             break;
+
+        case 'user':
+            if (parts.length < 2) {
+                console.log('Please specify username');
+                break;
+            }
+            const username = parts[1].toLowerCase();
+            const userTodos = lines.filter(todo => 
+                todo.author && todo.author.toLowerCase() === username
+            );
+            for (const line of userTodos){
+                console.log(line.text)
+            }
+            break;
         default:
             console.log('wrong command');
             break;
@@ -56,3 +85,4 @@ function processCommand(command) {
 }
 
 // TODO you can do it!
+
