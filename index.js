0674614231323f8871246b7da845e341bb6b2f8a
Lychee <2006tka@gmail.com>
2026-03-03 19:29:26 +0500

    1 done

--- index.js
+++ index.js
@@ -11,9 +11,27 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getComments() {
+    const comments = [];
+    for (const content of files) {
+        const lines = content.split('\n');
+        for (const line of lines) {
+            const match = line.match(/\/\/\s*TODO\s*(.*)/);
+            if (match) {
+                comments.push(match[1]);
+            }
+        }
+
+
+    }
+    console.log(comments)
+    return comments;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
+            getComments();
             process.exit(0);
             break;
         default:
